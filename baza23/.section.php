8fb317f14d50e516815ee048ca75fc7415815a01
Yury Egoshin <me@egoshin.net>
2016-02-24 21:49:42 +0300

    Инициализация проекта 1C-Bitrix

---
+++ baza23/.section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Креативное агентство baza23',
+	'description' => 'Креативное агентство baza23',
+	'keywords' => 'Keywords',
+	'robots' => 'index, follow'
+);
+?>
---
+++ shared/bitrix/templates/baza23/description.php
@@ -0,0 +1,8 @@
+<?
+$arTemplate = array (
+  'NAME' => 'baza23',
+  'DESCRIPTION' => 'Шаблон сайта креативного агентсва baza23',
+  'SORT' => '',
+  'TYPE' => '',
+);
+?>
