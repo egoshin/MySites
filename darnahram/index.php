66b270bf37de13ff859aad0c2dc8d2057ff3ccaa
Yury Egoshin <me@egoshin.net>
2016-02-29 21:45:22 +0300

    Создание шаблона darnahram в 1C-Bitrix

--- darnahram/index.php
+++ darnahram/index.php
@@ -1,8 +1,61 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
-?> 
-<h1>darnahram</h1>
+?>
+<h1>Храм Крестовоздвижения, с. Дарна</h1>
+<h2>Храм Крестовоздвижения, с. Дарна</h2>
+<h3>Храм Крестовоздвижения, с. Дарна</h3>
+<h4>Храм Крестовоздвижения, с. Дарна</h4>
+<h5>Храм Крестовоздвижения, с. Дарна</h5>
+<h6>Храм Крестовоздвижения, с. Дарна</h6>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aperiam aut blanditiis culpa cupiditate deserunt
+    dolore eius, exercitationem harum illum in ipsam nam necessitatibus neque nostrum officia omnis perferendis
+    praesentium provident ratione rem rerum tempore unde ut voluptatum! A ab corporis dolores eaque eveniet,
+    exercitationem modi nihil nisi quae quos rem tempora vel! <a href="#">Это ссылка куда-либо</a> Culpa debitis omnis
+    quis reprehenderit ullam? Atque
+    corporis iste neque quo quod similique tempora ullam vel! Ab accusantium aspernatur consequuntur corporis culpa
+    cumque cupiditate earum eum fugiat, illo iusto labore neque nisi odit officia porro quis repellendus repudiandae
+    rerum sint soluta sunt temporibus ullam unde, voluptatibus. Numquam?</p>
+<p class="small">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, aperiam aut blanditiis culpa cupiditate deserunt
+    dolore eius, exercitationem harum illum in ipsam nam necessitatibus neque nostrum officia omnis perferendis
+    praesentium provident ratione rem rerum tempore unde ut voluptatum! A ab corporis dolores eaque eveniet,
+    exercitationem modi nihil nisi quae quos rem tempora vel! <a href="#">Это ссылка куда-либо</a> Culpa debitis omnis
+    quis reprehenderit ullam? Atque
+    corporis iste neque quo quod similique tempora ullam vel! Ab accusantium aspernatur consequuntur corporis culpa
+    cumque cupiditate earum eum fugiat, illo iusto labore neque nisi odit officia porro quis repellendus repudiandae
+    rerum sint soluta sunt temporibus ullam unde, voluptatibus. Numquam?
+</p>
+<div class="row">
+    <div class="col-xs-12 col-sm-6">
+        <ul>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+            <li>Пункт ненумерованного списка</li>
+        </ul>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+        <ol>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+            <li>Пункт нумерованного списка</li>
+        </ol>
+    </div>
+</div>
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
--- shared/bitrix/templates/darnahram/footer.php
+++ shared/bitrix/templates/darnahram/footer.php
@@ -1,6 +1,38 @@
 <?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-	die();
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+    die();
 ?>
-	</body>
+</div>
+</div>
+<div class="hFooter visible-xs visible-sm"></div>
+</div>
+<footer class="visible-xs visible-sm">
+    <div class="footer container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 text-center">
+                <ul class="list-inline social">
+                    <li><a href="https://www.facebook.com/darnahram/" target="_blank"><i
+                                class="fa fa-facebook fa-lg"></i></a></li>
+                    <li><a href="https://twitter.com/darnahram" target="_blank"><i class="fa fa-twitter fa-lg"></i></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCx8M8UNI6MBUZsAYCTCXzww" target="_blank"><i
+                                class="fa fa-youtube fa-lg"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-4 text-center">
+                <div class="phone-mail">
+                    <? $phone = '+7 495 994-56-29'; ?>
+                    <a href="tel:+<?= preg_replace("#[^\d]#", "", $phone) ?>"><?= $phone ?></a><br>
+                    <a href="mailto:info@darnahram.ru">info@darnahram.ru</a>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 text-center">
+                <div class="baza23">
+                    <a href='http://www.baza23.ru' target='_blank'><?= GetMessage("FOOTER_CREATOR_TEXT") ?>
+                        &nbsp;<?= GetMessage("FOOTER_CREATOR_HREF") ?></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
 </html>
